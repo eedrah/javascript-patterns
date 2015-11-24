cb2171f7179192c7f8b3a8c622f546836359217f
Eedrah <eedrah@users.noreply.github.com>
2015-11-24 15:54:33 +1300

    make into basic strategy

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
 /* eslint no-console:0 */
 
 var cookMeal = function (food) {
-    switch(food) {
-        case 'egg':
-            boil(food);
-            break;
-        case 'bread':
-            toast(food);
-            break;
-        case 'hashbrown':
-            toast(food);
-            break;
-        case 'potato':
-            boil(food);
-            break;
-        default:
-            raw(food);
-    }
+    var methods = [
+        Boil,
+        Toast,
+        Raw
+    ];
+    var chosenMethod;
+    methods.forEach(function (method) {
+        if (method.canCook(food)) {
+            chosenMethod = method;
+        }
+    });
+    chosenMethod.cook(food);
 };
 
-var boil = function (food) {
-    console.log('boiled ' + food);
-};
-
-var toast = function (food) {
-    console.log('toasted ' + food);
-};
-
-var raw = function (food) {
-    console.log('rawed ' + food);
-};
+var Boil = (function () {
+    var foods = [
+        'egg',
+        'potato'
+    ];
+    var cook = function (food) {
+        console.log('Boiled ' + food);
+    };
+    var canCook = function (food) {
+        return (foods.indexOf(food) >= 0);
+    };
+    return {
+        cook: cook,
+        canCook: canCook
+    };
+}());
+var Toast = (function () {
+    var foods = [
+        'bread',
+        'hashbrown'
+    ];
+    var cook = function (food) {
+        console.log('Toasted ' + food);
+    };
+    var canCook = function (food) {
+        return (foods.indexOf(food) >= 0);
+    };
+    return {
+        cook: cook,
+        canCook: canCook
+    };
+}());
+var Raw = (function () {
+    var badFoods = [
+        'egg',
+        'potato',
+        'bread',
+        'hashbrown'
+    ];
+    var cook = function (food) {
+        console.log('Rawed ' + food);
+    };
+    var canCook = function (food) {
+        return (badFoods.indexOf(food) == -1);
+    };
+    return {
+        cook: cook,
+        canCook: canCook
+    };
+}());
 
 cookMeal('egg');
 cookMeal('potato');
